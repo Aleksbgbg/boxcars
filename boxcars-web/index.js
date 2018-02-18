a07357c891eca94c8f9a3ec0f78fcd31401b419d
Nick Babcock <nbabcock19@hotmail.com>
2018-02-18 14:22:45 -0500

    Wasm-bindgen supports slices

--- boxcars-web/index.js
+++ boxcars-web/index.js
@@ -1,9 +1,11 @@
-import * as wasm from "./boxcars_web";
-//import { parse_replay } from "./boxcars_web";
-import { booted, memory } from "./boxcars_web_wasm";
+import { parse_replay, WebReplay } from "./boxcars_web";
+//import { parse_replay, greet } from "./boxcars_web";
+import { booted } from "./boxcars_web_wasm";
 
 booted.then(() => {
-  window.my_memory = memory;
+  window.parse_replay = parse_replay;
+  window.WebReplay = WebReplay;
+/*  window.my_memory = memory;
   window.wasm = wasm;
-  window.boxcars = wasm.parse_replay;
+  window.boxcars = wasm.parse_replay;*/
 });
--- boxcars-web/src/lib.rs
+++ boxcars-web/src/lib.rs
@@ -3,24 +3,38 @@ extern crate boxcars;
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
-use boxcars::{ParserBuilder};
-use std::slice;
+use boxcars::{ParserBuilder, Replay};
 
-/*
 #[wasm_bindgen]
 #[derive(Default)]
 pub struct WebReplay {
-    pub header_size: i32,
-    pub header_crc: i32,
-    pub major_version: i32,
-    pub minor_version: i32,
+    header_size: i32,
+    header_crc: i32,
+    major_version: i32,
+    minor_version: i32,
+    game_type: String,
 }
 
+#[wasm_bindgen]
 impl WebReplay {
-    pub fn new(int: i32) -> WebReplay {
-        WebReplay {
-            header_size: int,
-        }
+	pub fn header_size(&self) -> i32 {
+        self.header_size
+    }	
+
+	pub fn header_crc(&self) -> i32 {
+        self.header_crc
+    }
+
+	pub fn major_version(&self) -> i32 {
+        self.major_version
+    }
+
+	pub fn minor_version(&self) -> i32 {
+        self.minor_version
+    }
+
+	pub fn game_type(&self) -> String {
+        self.game_type.clone()
     }
 }
 
@@ -31,21 +45,21 @@ impl<'a> From<Replay<'a>> for WebReplay {
             header_crc: replay.header_crc,
             major_version: replay.major_version,
             minor_version: replay.minor_version,
+            game_type: replay.game_type.to_owned().to_string(),
         }
     }
-}*/
+}
 
 #[no_mangle]
 #[wasm_bindgen]
-pub extern fn parse_replay(ptr: *mut u8, len: usize) -> String {
-    let slice = unsafe { slice::from_raw_parts(ptr, len) };
-    let parsing = ParserBuilder::new(&slice[..])
+pub extern fn parse_replay(data: &[u8]) -> WebReplay {
+    let parsing = ParserBuilder::new(data)
         .always_check_crc()
         .must_parse_network_data()
         .parse();
     
     match parsing {
-        Ok(replay) => format!("We did it: {}", replay.major_version),
-        Err(ref e) => format!("error: {}", e),
+        Ok(replay) => WebReplay::from(replay),
+        Err(ref e) => WebReplay::default(),
     }
 }
